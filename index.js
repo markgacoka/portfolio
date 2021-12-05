62b04c3c40b600a72e0056bd92b403c6f98622aa
Mark Gacoka Mbui <markgacoka@gmail.com>
2021-12-05 16:39:54 +0100

    Sliding window bug fix

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let menuToggle = document.querySelector('.toggle');
 let navigation = document.querySelector('.navigation');
 let sec = document.querySelector('.sec');
+let sechome = document.querySelector('.sechome');
 
 menuToggle.onclick = function(){
     menuToggle.classList.toggle('active');
     navigation.classList.toggle('active');
-    sec.classList.toggle('active');
+    if (navigation.classList.contains('active') && sec !== null) {
+        sec.style.display = 'none';
+    } else {
+        sechome.style.display = 'none';
+    }
 }
